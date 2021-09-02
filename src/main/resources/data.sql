5ef9278c598fe28e866abcca1a70f7dc87fb0c66
clinomics-dlee <dlee@clinomics.co.kr>
2021-09-02 16:16:49 +0900

    Test 권한 추가 및 left 메뉴 권한 적용

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,6 +13,8 @@ SELECT 'ROLE_EXP_40' AS code, '검사실관리자', 0, 1 AS name FROM DUAL WHERE
 INSERT INTO role (code, name, is_personal_view, in_use)
 SELECT 'ROLE_EXP_80' AS code, '검사실책임자', 1, 1 AS name FROM DUAL WHERE NOT EXISTS (SELECT * FROM role WHERE code = 'ROLE_EXP_80');
 INSERT INTO role (code, name, is_personal_view, in_use)
+SELECT 'ROLE_TEST_90' AS code, 'TESTER', 0, 1 AS name FROM DUAL WHERE NOT EXISTS (SELECT * FROM role WHERE code = 'ROLE_TEST_90');
+INSERT INTO role (code, name, is_personal_view, in_use)
 SELECT 'ROLE_IT_99' AS code, 'IT관리자', 1, 1 AS name FROM DUAL WHERE NOT EXISTS (SELECT * FROM role WHERE code = 'ROLE_IT_99');
 INSERT INTO member (id, dept, email, is_failed_mail_sent, in_use, name, password)
 SELECT 'admin', 'IT Infra', 'info@clinomics.com', 1, 1, 'Administrator', '$10$8y7E2JJg7d68OQSFKw5rmePUCEd5NtyCKhoX5Ue.0n46veUaHw6Oq' FROM DUAL WHERE NOT EXISTS (SELECT * FROM member WHERE id = 'admin');
