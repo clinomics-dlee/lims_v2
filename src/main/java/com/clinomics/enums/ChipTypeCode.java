ac799dbe42485c03f52937ae3a82b724913b0451
clinomics-dlee <dlee@clinomics.co.kr>
2023-09-04 15:43:34 +0900

    APMRA Chip 실행 명령어 변경

--- src/main/java/com/clinomics/enums/ChipTypeCode.java
+++ src/main/java/com/clinomics/enums/ChipTypeCode.java
@@ -1,7 +1,8 @@
 package com.clinomics.enums;
 
 public enum ChipTypeCode {
-	APMRA_CHIP("APMRA Chip", "(Axiom_KORV1-1)", "/BiO/Research/Cel2GSSInput/Script2/runAnalysis_APMRA.py"),
+	// APMRA_CHIP("APMRA Chip", "(Axiom_APMRA_24)", "/BiO/Research/Cel2GSSInput/Script2/runAnalysis_APMRA.py"),
+	APMRA_CHIP("APMRA Chip", "(Axiom_APMRA_24)", "/BiO/Research/Cel2GSSInput/Script2_APMRA_U10K/10.run_APMRA2Genotypes.py"),
 	CUSTOM_CHIP("Custom Chip", "(Axiom_GSChip-1)", "/BiO/Research/Cel2GSSInput/Script2/runAnalysis_GSChip.py"),
 	CUSTOM_CHIP2("Custom Chip2", "(Axiom_GSChip-2)", "/BiO/Research/Cel2GSSInput/Script2/runAnalysis_GSChip2.py")
 	;
