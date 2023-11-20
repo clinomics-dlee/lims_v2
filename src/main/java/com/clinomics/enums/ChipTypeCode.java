335bd408d21e6265dce13b2651d1b6cd4d9ecaf9
clinomics-dlee <dlee@clinomics.co.kr>
2023-11-20 15:08:52 +0900

    Custom chip 3 명령어 변경

--- src/main/java/com/clinomics/enums/ChipTypeCode.java
+++ src/main/java/com/clinomics/enums/ChipTypeCode.java
@@ -5,7 +5,7 @@ public enum ChipTypeCode {
 	APMRA_CHIP("APMRA Chip", "(Axiom_APMRA_24)", "python3", "/BiO/Research/Cel2GSSInput/Script2_APMRA_U10K/10.run_APMRA2Genotypes.py"),
 	CUSTOM_CHIP("Custom Chip", "(Axiom_GSChip-1)", "python", "/BiO/Research/Cel2GSSInput/Script2/runAnalysis_GSChip.py"),
 	CUSTOM_CHIP2("Custom Chip2", "(Axiom_GSChip-2)", "python", "/BiO/Research/Cel2GSSInput/Script2/runAnalysis_GSChip2.py"),
-	CUSTOM_CHIP3("Custom Chip3", "(Axiom_GSChip_3)", "python3", "/BiO/Research/Cel2GSSInput_GSChip3/Script/runAnalysis_GSChip3.1.py")
+	CUSTOM_CHIP3("Custom Chip3", "(Axiom_GSChip_3)", "python3", "/BiO/Research/Cel2GSSInput_GSChip3/Script/runAnalysis_GSChip3.py")
 	;
 
 	private final String value;
